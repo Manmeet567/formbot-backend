84f3c08d82e4bd890a0347bd697f19141a6a4f67
Manmeet567 <manmeet567singh@gmail.com>
2024-12-24 20:03:37 +0530

    Modify: Modified form Model

--- models/formModel.js
+++ models/formModel.js
@@ -7,15 +7,19 @@ const FormSchema = new mongoose.Schema({
     ref: "Folder",
     default: null,
   },
+  workspaceId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Workspace",
+    default: null,
+  },
   title: { type: String, required: true },
-  fields: [
+  flow: [
     {
-      label: { type: String, required: true },
-      type: { type: String, required: true },
-      required: { type: Boolean, default: false },
+      field: { type: String, required: true }, // Field type (e.g., text, number)
+      type: { type: String, enum: ["bubble", "input"], required: true }, // Only 'bubble' or 'input'
+      fieldValue: { type: mongoose.Schema.Types.Mixed, default: null }, // Default value is null
     },
   ],
-  responses: { type: Array, default: [] },
   createdAt: { type: Date, default: Date.now },
 });
 
